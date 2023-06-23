8253bd9359b6b4fad3b6af6db1de05f45f8694e5
Gaurav Sharma <gsharma.nus@gmail.com>
2023-06-23 12:06:04 +0800

    ⚡️Add linkedin contact section

---
+++ docs/background/components/LinkedInContacts.js
@@ -0,0 +1,54 @@
+import { Header, Icon } from 'semantic-ui-react';
+import styled from '@emotion/styled';
+import { xs } from '../../../src/utils';
+
+export const LinkedInContacts = () => (
+  <Wrapper>
+    {[
+      {
+        name: 'Noorul',
+        link: 'https://www.linkedin.com/in/noorulameen-dev/',
+      },
+      { name: 'Gaurav', link: 'https://www.linkedin.com/in/gaurav-sh/' },
+      { name: 'Benedita', link: 'https://www.linkedin.com/in/beneditatan/' },
+    ].map((contact) => (
+      <ContactContainer>
+        <HeaderWrapper>
+          <Header as="h3">{contact.name}</Header>
+        </HeaderWrapper>
+        <Icon name="linkedin" size="large" link />
+      </ContactContainer>
+    ))}
+  </Wrapper>
+);
+
+const HeaderWrapper = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background: #e7e7e7;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 25px;
+
+  ${xs} {
+    flex-direction: column;
+  }
+`;
+
+const ContactContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  ${xs} {
+    margin-bottom: 20px;
+  }
+`;
